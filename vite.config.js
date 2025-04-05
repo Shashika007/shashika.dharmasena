567c874fd1607ee457858b474e71233effb2e2c4
Shashika <Shashika@pic.net.tw>
2025-04-05 08:54:38 +0530

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/shashika.dharmasena",
+  build: {
+    rollupOptions: {
+      external: [
+        '/shashika.dharmasena/assets/shashika.jpg'
+      ]
+    }
+  }
 })
